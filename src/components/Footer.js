171ebd0532c0ee52c67dc321b0a5df33daf0ef96
vaibhav shukla <157266198+imvaibhav100@users.noreply.github.com>
2026-02-14 12:51:33 +0530

    Remove Instagram icon from Footer component
    Removed Instagram link and icon from the footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import {
   AiFillGithub,
   AiOutlineTwitter,
-  AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
@@ -51,16 +50,7 @@ function Footer() {
                 <FaLinkedinIn />
               </a>
             </li>
-            <li className="social-icons">
-              <a
-                href="https://www.instagram.com/VAIBHAV4419"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
+            
           </ul>
         </Col>
       </Row>
